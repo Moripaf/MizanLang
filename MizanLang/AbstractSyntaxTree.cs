59d39344dd962031b6f8951593ddc87bdcb8c465
moripaf <moripaf@outlook.com>
2026-02-27 14:43:50 +0330

    added initial AST

---
+++ MizanLang/AbstractSyntaxTree.cs
@@ -0,0 +1,111 @@
+namespace MizanLang.AST;
+
+using System.Collections.Immutable;
+using Sawmill;
+
+public class Rule
+{
+    public Expression Filter { get; }
+    public Expression Requirement { get; }
+
+    public Rule(Expression filter, Expression requirement)
+    {
+        Filter = filter;
+        Requirement = requirement;
+    }
+}
+
+public enum BinaryOperator
+{
+    Or, And,
+    Add, Subtract, Multiply, Divide, Modulo,
+    GreaterThan, LessThan, Equal, NotEqual, GreaterThanOrEqual, LessThanOrEqual
+}
+
+public abstract class Expression: IRewritable<Expression>
+{
+    public abstract int CountChildren();
+    public abstract void GetChildren(Span<Expression> childrenReceiver);
+    public abstract Expression SetChildren(ReadOnlySpan<Expression> newChildren);
+}
+
+public class BinaryExpression(Expression left, BinaryOperator op, Expression right) : Expression
+{
+    public Expression Left { get; } = left;
+    public BinaryOperator Operator { get; } = op;
+    public Expression Right { get; } = right;
+
+    public override int CountChildren() => 2;
+
+    public override void GetChildren(Span<Expression> childrenReceiver)
+    {
+        childrenReceiver[0] = Left;
+        childrenReceiver[1] = Right;
+    }
+
+    public override Expression SetChildren(ReadOnlySpan<Expression> newChildren)
+        => new BinaryExpression(newChildren[0], Operator, newChildren[1]);
+}
+
+public class InListExpression(Expression target, ImmutableArray<Expression> values) : Expression
+{
+    public Expression Target { get; } = target;
+    public ImmutableArray<Expression> Values { get; } = values;
+
+    public override int CountChildren() => 1 + Values.Length;
+
+    public override void GetChildren(Span<Expression> childrenReceiver)
+    {
+        childrenReceiver[0] = Target;
+        for (int i = 0; i < Values.Length; i++)
+        {
+            childrenReceiver[i + 1] = Values[i];
+        }
+    }
+
+    public override Expression SetChildren(ReadOnlySpan<Expression> newChildren)
+    {
+        var newTarget = newChildren[0];
+        var newValues = newChildren.Slice(1).ToArray().ToImmutableArray();
+        return new InListExpression(newTarget, newValues);
+    }
+}
+
+public class BetweenExpression(Expression target, Expression lowerBound, Expression upperBound)
+    : Expression
+{
+    public Expression Target { get; } = target;
+    public Expression LowerBound { get; } = lowerBound;
+    public Expression UpperBound { get; } = upperBound;
+
+    public override int CountChildren() => 3;
+
+    public override void GetChildren(Span<Expression> childrenReceiver)
+    {
+        childrenReceiver[0] = Target;
+        childrenReceiver[1] = LowerBound;
+        childrenReceiver[2] = UpperBound;
+    }
+
+    public override Expression SetChildren(ReadOnlySpan<Expression> newChildren)
+        => new BetweenExpression(newChildren[0], newChildren[1], newChildren[2]);
+}
+
+public class IdentifierExpression(string name) : Expression
+{
+    public string Name { get; } = name;
+
+    public override int CountChildren() => 0;
+    public override void GetChildren(Span<Expression> childrenReceiver) { }
+    public override Expression SetChildren(ReadOnlySpan<Expression> newChildren) => this;
+}
+
+public class LiteralExpression(object value) : Expression
+{
+    // Value can hold int, double, string, or bool natively
+    public object Value { get; } = value;
+
+    public override int CountChildren() => 0;
+    public override void GetChildren(Span<Expression> childrenReceiver) { }
+    public override Expression SetChildren(ReadOnlySpan<Expression> newChildren) => this;
+}
